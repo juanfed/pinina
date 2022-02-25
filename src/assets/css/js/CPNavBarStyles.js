546747c70a4f286b97e73e91123d18f2006bcbe8
Oscar Rodriguez <oscarstep.123@gmail.com>
2022-02-25 11:26:58 -0500

    Se limpia repositorio

--- src/assets/css/js/CPNavBarStyles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core';
-
-export const CPNavbarStyles = makeStyles((theme) => ({
-	root: {
-		padding: theme.spacing(2, 1, 1, 1)
-	}
-}));
-
-export default CPNavbarStyles;
