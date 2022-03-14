c36376c641dc942fda9807568199068146e4fafa
José Luis <ingjoseluisbaron@gmail.com>
2022-03-14 09:35:17 -0500

    Cambios redes

--- src/components/auth/FacebookAuth.js
+++ src/components/auth/FacebookAuth.js
@@ -1,47 +1,39 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
+import React from "react";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 
-  // Snackbar
-  import { useSnackbar } from "notistack";
-  import { useRouter } from "next/router";
-
+// Snackbar
+import { useSnackbar } from "notistack";
+import { useRouter } from "next/router";
 
 const FacebookAuth = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const router = useRouter();
 
-    const { enqueueSnackbar } = useSnackbar();
-    const router = useRouter();
-  
+  const respuestaFacebook = (respuesta) => {
+    /* console.log(respuesta); */
 
-    const respuestaGoogle = (respuesta)=> {
-        /* console.log(respuesta); */
-        
-        if (respuesta.accessToken) {
-            
-            enqueueSnackbar("Bienvenido a PetPinina", { variant: 'success' });
-            router.push('/petsAndClients');
-                
-    
-          } else {
-            enqueueSnackbar(response.msg, { variant: "error" });}
-          
+    if (respuesta.accessToken) {
+      enqueueSnackbar("Bienvenido a PetPinina", { variant: "success" });
+      router.push("/petsAndClients");
+    } else {
+      enqueueSnackbar(response.msg, { variant: "error" });
     }
-    return ( 
+  };
+  return (
+    <div>
+      <FacebookLogin
+        appId="651206472658419"
+        autoLoad={false}
+        callback={respuestaFacebook}
+        fields="name,email,picture"
+        render={(renderProps) => (
+          <a onClick={renderProps.onClick} class="googleRegistro">
+            Ingresar con Facebook
+          </a>
+        )}
+      />
+    </div>
+  );
+};
 
-        <div>
-   
-        <GoogleLogin
-    clientId="516929823924-svb8qslcllhfd6r8fesb2sh6tpgmo62a.apps.googleusercontent.com"
-    /* buttonText="Ingresar con Google" */
-
-    render={renderProps => (
-        <a  onClick={renderProps.onClick} disabled={renderProps.disabled} class="googleRegistro">Ingresar con Facebook</a>
-      )}
-    onSuccess={respuestaGoogle}
-    onFailure={respuestaGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
-  </div>
-     );
-}
- 
 export default FacebookAuth;
