a7ad409bc2a1895e3b16392362062cd966979673
José Luis <ingjoseluisbaron@gmail.com>
2022-03-29 16:45:21 -0500

    Menu

--- src/components/floatMenu/index.jsx
+++ src/components/floatMenu/index.jsx
@@ -21,7 +21,28 @@ const FloatMenu = ({ buttons }) => {
         <Draggable onDrag={ (e, data) => handleDrag(e, data) }>
             <main className="floatmenu">
                 <section className={ active ? "hidden-circles" : "intern-circles" }>
-               
+                <div className="loader">
+                            <span className="dot dot1"></span>
+                            <span className="dot dot2"></span>
+                            <span className="dot dot3"></span>
+                            <span className="dot dot4"></span>
+                            <span className="dot dot5"></span>
+                            <span className="dot dot6"></span>
+                            <span className="dot dot7"></span>
+                            <span className="dot dot8"></span>
+                            <span className="dot dot9"></span>
+                            <span className="dot dot10"></span>
+                            <span className="dot dot11"></span>
+                            <span className="dot dot12"></span>
+                            <span className="dot dot13"></span>
+                            <span className="dot dot14"></span>
+                            <span className="dot dot15"></span>
+                            <span className="dot dot16"></span>
+                            <span className="dot dot17"></span>
+                            <span className="dot dot18"></span>
+                            <span className="dot dot19"></span>
+                            <span className="dot dot20"></span>
+                            </div>   
                 <img src="/img/circuloFondo.svg" className="circle-6" width="100%" alt="" />
                     <img src="/img/circle-1.svg" className="circle-1" width="100%" alt="" />
                     <img src="/img/circle-2.svg" className="circle-2" width="100%" alt="" />
@@ -29,6 +50,8 @@ const FloatMenu = ({ buttons }) => {
                     <img src="/img/circle-5.svg" className="circle-5" width="100%" alt="" />
                     <img src="/img/circle-7.svg" className="circle-8" width="100%" alt="" />
                     <img src="/img/profundidad.svg" className="circle-7" width="100%" alt="" /> 
+
+               
                 
                 </section>
                 <img 
